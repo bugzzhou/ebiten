5b9ba1ed8321f0c576e395b1371d309d5732f061
jingsen_zhou <jingsen_zhou@maxnetsys.com.cn>
2024-07-31 17:59:55 +0800

    尝试数字键打牌-还未测试

--- scene/combatScene/update.go
+++ scene/combatScene/update.go
@@ -74,3 +74,59 @@ func EnemyAct(g *Game) {
 	g.Enemy.EnemyAct(g.Round, g.Character)
 	g.Round += 1
 }
+
+// func ChangeStatus(g *Game) {
+// 	// 检查是否按下数字键
+// 	for i := ebiten.Key1; i <= ebiten.Key0; i++ {
+// 		if inpututil.IsKeyJustPressed(i) {
+// 			index := int(i - ebiten.Key1)
+// 			if index < len(g.Character.HandCards) {
+// 				g.IsDragging = true
+// 				g.DraggingIndex = index
+// 				g.ExpandIndex = -1
+// 				return
+// 			}
+// 		}
+// 	}
+
+// 	// 检查鼠标左键点击
+// 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+// 		index := getExpandIndex(len(g.Character.HandCards))
+// 		if index != -1 {
+// 			g.IsDragging = true
+// 			g.DraggingIndex = index
+// 			g.ExpandIndex = -1
+// 		} else {
+// 			return
+// 		}
+// 	}
+
+// 	// 拖动过程中
+// 	if g.IsDragging {
+// 		// 检查鼠标左键松开
+// 		if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+// 			if isMouseOverEnemy() && enemyIsEnough(g, g.DraggingIndex) {
+// 				g.Character.PlayCard(g.DraggingIndex, &g.Enemy)
+// 			}
+// 			g.IsDragging = false
+// 			g.DraggingIndex = -1
+// 		}
+// 	}
+
+// 	// 更新卡牌悬停状态
+// 	if !g.IsDragging {
+// 		g.ExpandIndex = getExpandIndex(len(g.Character.HandCards))
+// 	}
+// }
+
+// func isMouseOverEnemy() bool {
+// 	x, y := ebiten.CursorPosition()
+// 	enemyX, enemyY := GetXY(EnemyPos)
+// 	return x >= int(enemyX) && x <= int(enemyX)+imageWidth && y >= int(enemyY) && y <= int(enemyY)+imageHeight
+// }
+
+// func enemyIsEnough(g *Game, index int) bool {
+// 	c := g.Character.HandCards[index]
+// 	fmt.Printf("energy, cost are: %v, %v\n", g.Character.Energy, c.Cost)
+// 	return g.Character.Energy >= c.Cost
+// }
