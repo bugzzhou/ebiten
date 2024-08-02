c178374045aa7087cd1d61c633a1024628061ede
jingsen_zhou <jingsen_zhou@maxnetsys.com.cn>
2024-08-02 18:03:37 +0800

    修改

---
+++ sample/clockk/game.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 640
+	gridSize     = 100
+)
+
+var (
+	grid       = make([][]bool, gridSize)
+	cellSize   = screenWidth / gridSize
+	stepButton = &Button{
+		x:      screenWidth - 300,
+		y:      screenHeight - 40,
+		width:  80,
+		height: 30,
+		label:  "Step",
+	}
+	runButton = &Button{
+		x:      screenWidth - 200,
+		y:      screenHeight - 40,
+		width:  80,
+		height: 30,
+		label:  "Run",
+	}
+	stopButton = &Button{
+		x:      screenWidth - 100,
+		y:      screenHeight - 40,
+		width:  80,
+		height: 30,
+		label:  "Stop",
+	}
+	mousePressedLastFrame bool
+	changedCells          = make(map[Point]bool)
+	running               bool
+	ticker                *time.Ticker
+)
+
+type Point struct {
+	X, Y int
+}
+
+type Button1 struct {
+	x, y          int
+	width, height int
+	label         string
+}
+
+// 函数部分
+type Game struct{}
+
+func init() {
+	for i := range grid {
+		grid[i] = make([]bool, gridSize)
+	}
+}
+
+func NewGame() (*Game, error) {
+	return &Game{}, nil
+}
+
+func (g *Game) Update() error {
+	changeStatus()
+	if running {
+		select {
+		case <-ticker.C:
+			updateGrid()
+		default:
+		}
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	drawUI(screen)
+
+	stepButton.Draw(screen)
+	runButton.Draw(screen)
+	stopButton.Draw(screen)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
---
+++ sample/clockk/update.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func changeStatus() {
+	mousePressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	x, y := ebiten.CursorPosition()
+	gridX, gridY := x/cellSize, y/cellSize
+
+	if mousePressed {
+		if gridX < gridSize && gridY < gridSize {
+			point := Point{X: gridX, Y: gridY}
+			if !changedCells[point] {
+				grid[gridX][gridY] = !grid[gridX][gridY]
+				changedCells[point] = true
+			}
+		}
+	} else {
+		changedCells = make(map[Point]bool) // 重置记录
+	}
+
+	if mousePressed && !mousePressedLastFrame {
+		if stepButton.IsClicked(x, y) {
+			updateGrid() // 单次迭代
+		} else if runButton.IsClicked(x, y) {
+			running = true
+			ticker = time.NewTicker(100 * time.Millisecond) // 每秒钟迭代一次
+		} else if stopButton.IsClicked(x, y) {
+			running = false
+			if ticker != nil {
+				ticker.Stop()
+			}
+		}
+	}
+
+	mousePressedLastFrame = mousePressed
+}
+
+func (b *Button) IsClicked(x, y int) bool {
+	return x >= b.x && x <= b.x+b.width && y >= b.y && y <= b.y+b.height
+}
+
+func updateGrid() {
+	newGrid := make([][]bool, gridSize)
+	for i := range newGrid {
+		newGrid[i] = make([]bool, gridSize)
+	}
+
+	for i := 0; i < gridSize; i++ {
+		for j := 0; j < gridSize; j++ {
+			num := countNeighbors(i, j)
+			if !grid[i][j] && num == 3 {
+				newGrid[i][j] = true
+			} else if grid[i][j] {
+				if num == 2 || num == 3 {
+					newGrid[i][j] = true
+				} else {
+					newGrid[i][j] = false
+				}
+			}
+		}
+	}
+
+	grid = newGrid
+}
+
+func countNeighbors(row, col int) int {
+	count := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			tRow := row + i
+			tCol := col + j
+			if tRow == row && tCol == col {
+				continue
+			}
+			if tRow >= 0 && tRow < gridSize && tCol >= 0 && tCol < gridSize {
+				if grid[tRow][tCol] {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
